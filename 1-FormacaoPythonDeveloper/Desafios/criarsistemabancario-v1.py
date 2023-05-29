5d194281844268ba5c0aaeee262141c576d9931f
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-05-29 16:13:58 -0300

    desafio de conta bancária

--- 1-FormacaoPythonDeveloper/Desafios/criarsistemabancario-v1.py
+++ 1-FormacaoPythonDeveloper/Desafios/criarsistemabancario-v1.py
@@ -19,32 +19,63 @@ while True:
   traco()
   print (menu)
   traco ()
-
-
-  opcao = int(input('Digite a opção: '))
   
+  print()
+  opcao = int(input('Digite a opção: '))
+  print()
 
   if opcao == 1:
     traco()
     print('DEPÓSITO')
-    deposito = float(input('Valor do depósito: R$ '))
-    if deposito <= 0:
-       print('Não é aceito depósito sendo 0 ou Valor Negativo')
-    else:   
-      saldo += deposito
+    valor = float(input('Valor do depósito: R$ '))
+
+    if valor > 0:
+       saldo += valor
+       extrato += f'Depósito: R$ {valor:.2f}\n'
+
+    else:
+       print('Falha na operação. Informe um valor válido.')
 
   elif opcao == 2:
-     print('sacar')
+    traco()
+    print('SACAR')
+    valor = float(input('Valor do saque: R$ '))
+
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saque = numero_saques >= LIMITE_SAQUES
+
+    if excedeu_saldo:
+       print('Falha na operação de Saque. Saldo insuficiente.')
+    
+    elif excedeu_limite:
+       print(f'Falha na operação de Saque. Limite R$ {limite}/saque.')
+    
+    elif excedeu_saque:
+       print(f'Falha na operação de Saque. {LIMITE_SAQUES}/dia.')
+         
+    elif valor > 0:
+       saldo -= valor
+       extrato += f'Saque R$ {saldo:.2f}\n'
+       numero_saques += 1
+
+
+    else:   
+      print('Falha na operação. Informe um valor válido.')
+
 
   elif opcao == 3:
-     print(extrato)
+     traco()
+     print('\n*************** EXTRATO ***************')
+     print('Sem movimentaçao na Conta' if not extrato else extrato)
+     print(f'\nSaldo: R$ {saldo:.2f}\n')
 
   elif opcao == 0:
       traco()
       print('Saindo do sistema, obrigao por utilizar!')
       break
+    
   else:
      print('Operação inválida. Por favor selecione novamente a operação desejada.')
 
 
-print(f'Valor do Saldo em conta corrente: {saldo:.2f}')
