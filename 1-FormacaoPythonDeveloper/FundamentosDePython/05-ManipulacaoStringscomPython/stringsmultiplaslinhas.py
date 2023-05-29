2f93616c25b84b8ad307c04fba3ef633ba656994
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-05-29 14:19:22 -0300

    desafios com python primeira parte

---
+++ 1-FormacaoPythonDeveloper/FundamentosDePython/05-ManipulacaoStringscomPython/stringsmultiplaslinhas.py
@@ -0,0 +1,18 @@
+nome = 'Flavio'
+
+print (f'''
+  Olá, meu nome é {nome},
+Estou aprendendo Python.
+  Essa mensagem tem diferentes recuos.
+''')
+
+print('''
+=========== MENU ===========
+  1 - Depositar
+  2 - Sacar
+  0 - Sair
+
+=======================================================
+
+          Obrigado por usar nosso sistema !!!
+''')
