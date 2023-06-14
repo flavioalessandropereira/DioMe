6a4ee7a67a6f868e1b8addc122cc84a3d8a0cc8d
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-06-14 11:23:33 -0300

    continuando o arquivo 2-herança multipla.py

--- 1-FormacaoPythonDeveloper/03-Programação Orientada a Objetos/32-AprendendoConceitoHerancaPython/02-heramca_multipla.py
+++ 1-FormacaoPythonDeveloper/03-Programação Orientada a Objetos/32-AprendendoConceitoHerancaPython/02-heramca_multipla.py
@@ -1,12 +1,48 @@
-class Foo:
-    def hello(self):
-        print(self.__class__.__name__.lower())
+class Animal:
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+
+    # mais automático para ver a classe, se adicionar vai aparecer automaticamente
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 
-class Bar(Foo):
-    def hello(self):
-        return super().hello()
+class Manifero(Animal):
+    def __init__(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
 
 
-bar = Bar()
-bar.hello()
+class Ave(Animal):
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
+
+
+
+class Gato(Manifero):
+    pass
+
+class FalarMixin:
+    def falar(self):
+       return 'Estou falando'
+
+
+class Ornitorrinco(Manifero, Ave, FalarMixin):
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        print(Ornitorrinco.__mro__) # __mro__, ordem de resolucao
+        print(Ornitorrinco.mro())
+
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+
+
+
+
+gato= Gato(nro_patas =4, cor_pelo ='preto')
+print(gato)
+
+
+ornitorrinco = Ornitorrinco(nro_patas=4, cor_pelo='vermelho', cor_bico='laranja') #usando **kw precisa informar, chave=valor Exemplo: cor_pelo = 'vermelho'
+print(ornitorrinco)
+print(ornitorrinco.falar())
