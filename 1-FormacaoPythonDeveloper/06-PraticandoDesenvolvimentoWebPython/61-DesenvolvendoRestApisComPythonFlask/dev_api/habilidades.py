86776ad7c92f8348d0132d95be6d2315c0dbfc25
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-06-28 11:12:22 -0300

    somente título de informaçao

--- 1-FormacaoPythonDeveloper/06-PraticandoDesenvolvimentoWebPython/61-DesenvolvendoRestApisComPythonFlask/dev_api/habilidades.py
+++ 1-FormacaoPythonDeveloper/06-PraticandoDesenvolvimentoWebPython/61-DesenvolvendoRestApisComPythonFlask/dev_api/habilidades.py
@@ -2,6 +2,7 @@ from flask_restful import Resource
 
 lista_habilidades = ['Python', 'Java', 'C++', 'VB', 'Rubi','Delphi', 'Flask']
 
+#A lista de Habilidades estará disposta no arquivo app_restful.py para ser utilizado
 class Habilidades(Resource):
     def get (self):
         return lista_habilidades
